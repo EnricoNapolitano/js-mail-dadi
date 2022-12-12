173b297411ab80eeb57ee733b9c326dc23ad59c1
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-12 15:25:48 +0100

    printing result match

--- script.js
+++ script.js
@@ -3,17 +3,27 @@ const cpuButton = document.getElementById("cpu-button");
 const maxNumber = 6;
 const playerNumber = document.getElementById("player-number");
 const cpuNumber = document.getElementById("cpu-number");
+const gameResult = document.getElementById ("game-result");
 
 playerButton.addEventListener('click', function(){
     const random = Math.random();
     let playerResult;
     playerResult = Math.floor(random * maxNumber) + 1;
-    playerNumber.innerText = `${playerResult}`;
+    let playerNumberPrinted = playerNumber.innerText = `${playerResult}`;
+
+    cpuButton.addEventListener('click', function(){
+        const random = Math.random();
+        let cpuResult;
+        cpuResult = Math.floor(random * maxNumber) + 1;
+        let cpuNumberPrinted = cpuNumber.innerText = `${cpuResult}`;
+
+        if (playerNumberPrinted > cpuNumberPrinted) {
+            gameResult.innerText = "complimenti, hai vinto!";
+        } else if (playerNumberPrinted === cpuNumberPrinted) {
+            gameResult.innerText = "è un pareggio! Non c'è nessun vincitore!";
+        } else {
+            gameResult.innerText = "mi dispiace, non hai vinto!";
+        }
+    })
 })
 
-cpuButton.addEventListener('click', function(){
-    const random = Math.random();
-    let cpuResult;
-    cpuResult = Math.floor(random * maxNumber) + 1;
-    cpuNumber.innerText = `${cpuResult}`;
-})
