0bcb9bc9355dcfe55597748869fae27703028697
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-12 15:09:45 +0100

    cpu number generator

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const playerButton = document.getElementById("player-button");
 const cpuButton = document.getElementById("cpu-button");
 const maxNumber = 6;
 const playerNumber = document.getElementById("player-number");
+const cpuNumber = document.getElementById("cpu-number");
 
 playerButton.addEventListener('click', function(){
     const random = Math.random();
     let playerResult;
     playerResult = Math.floor(random * maxNumber) + 1;
     playerNumber.innerText = `${playerResult}`;
+})
+
+cpuButton.addEventListener('click', function(){
+    const random = Math.random();
+    let cpuResult;
+    cpuResult = Math.floor(random * maxNumber) + 1;
+    cpuNumber.innerText = `${cpuResult}`;
 })
